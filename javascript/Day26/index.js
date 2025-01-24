b66a679013bba5735d0303ff5db5c3290cae9e90
Mahjabeen Akhtar <themahibabe@hotmail.com>
2025-01-24 11:32:16 +0500

    Add files via upload

---
+++ javascript/Day26/index.js
@@ -0,0 +1,56 @@
+
+
+function displayList (){
+    var list = `
+     <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+        <li>Item 4</li>
+    </ul>
+    `
+
+    document.getElementById("itemList").innerHTML = list
+}
+
+// Manipulationimages and text
+
+// function makeInvisible(){
+//     // document.getElementById("ugly").className = "hidden"
+//     document.getElementById("ugly").className += " hidden"
+//     document.getElementById("ugly").className += " hidden2"
+//     document.getElementById("ugly").className += " hidden3"
+// }
+
+
+// function newImage(){
+//     document.getElementById("image").src = "https://www.shutterstock.com/image-photo/violetpink-gerbera-flower-on-white-260nw-1088269616.jpg"
+// }
+// function oldImage(){
+//     document.getElementById("image").src = "https://images.pexels.com/photos/36753/flower-purple-lical-blosso.jpg?cs=srgb&dl=pexels-pixabay-36753.jpg&fm=jpg"
+
+// }
+
+
+// function copyLink (){
+//     var link = document.getElementById("link");
+//     var address = link.href;
+//     // console.log(address);
+    
+// }
+
+// document.getElementById("div").style.color = "red"
+// document.getElementById("div").style.background = "black"
+// document.getElementById("div").style = "color : 'red'"  // target
+
+
+
+// Target all elements by tag name
+
+
+// var allPara = document.getElementsByTagName("p");
+
+// document.getElementsByTagName("p").style.color = "yellow"
+
+// var allPara = document.getElementsByTagName("p");
+// allPara[0].style.color = "red"
